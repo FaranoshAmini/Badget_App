5bf2a0819b259556f4f290ccbb14251192711e1d
Faranosh Amini <faranosh.amini4@gmail.com>
2023-05-02 09:29:55 +0430

    Add relation between tables

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_and_belongs_to_many :entities
   validates :name, presence: true
   validates :icon, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  has_many :entities, foreign_key: 'author_id', dependent: :destroy
+  has_many :groups, foreign_key: 'author_id', dependent: :destroy
   validates :name, presence: true
 end

873163fc0ba3d6c054b057d3b4e7a6cd8983889e
Faranosh Amini <faranosh.amini4@gmail.com>
2023-05-03 18:28:52 +0430

    Update entities_controller.rb

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,7 +1,24 @@
 class EntitiesController < ApplicationController
-  def index; end
+  before_action :require_login
+  def index
+    @group = Group.find(params[:group_id])
+    @group_entities = Entity.where(group: @group).order(created_at: desc)
+  end
 
-  def new; end
+  def new
+    @new_entity = entity.new
+    @user_groups = Group.where(user: current_user)
+  end
 
-  def create; end
+  def create
+    @new_entity = Entity.new(entity_params)
+    return unless @new_entity.save
+    flash[:success] = 'Entity Created successfully'
+    redirect_to group_entities_path(entity_params[:group_id])
+  end
+
+  private
+  def entity_params
+    params.require(:entity).permit(:name, :amount, :author_id, :group_id)
+  end  
 end

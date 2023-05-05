193cb6ad2ef1b1c720fb974bdae7e7d029c09e05
Faranosh Amini <faranosh.amini4@gmail.com>
2023-05-05 10:44:12 +0430

    Update the database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,  :confirmable,
          :recoverable, :rememberable, :validatable
   has_many :entities, foreign_key: 'author_id'
   has_many :groups
-  validates :name, presence: true
+  # validates :name, presence: true
 end
